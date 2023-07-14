c9f887c85d16272654e6f8ade6a3557be18b73f5
JesúsT <83574910+JesusTrigo@users.noreply.github.com>
2023-07-14 14:16:34 +0200

    Update app.py

--- test/pages/app.py
+++ test/pages/app.py
@@ -1,40 +1,15 @@
 import streamlit as st
-import home
-from pages.detection import detection
-from pages.recomend import recomend, plots
-from pages.scrap import scrap
+from pages.recomend import plots
+from pages.recomend import app
 
-df_Beer = recomend.df_Beer
+def show():
+    # Mostrar enlaces para elegir entre plots.py y app.py
+    st.markdown("## Recommendations")
+    if st.button("Plots"):
+        plots.show()
+    elif st.button("App"):
+        app.run()
 
-def main():
-    # Configurando la barra lateral para seleccionar la página
-    st.sidebar.title('Selector de Páginas')
-    page = st.sidebar.radio('Seleccione una página', options=['Home', 'Detección', 'Recomendación', 'Scraping'])
-
-    # Dependiendo de la página seleccionada, mostrar el contenido correspondiente
-    if page == 'Home':
-        home.main()
-    elif page == 'Detección':
-        detection.main()
-    elif page == 'Recomendación':
-        st.sidebar.title('Selector de Recomendación') # Nuevo título para la sub-selección
-        recomend_option = st.sidebar.radio('Seleccione una opción', options=['App', 'Gráficos']) # Nuevo radio para sub-selección
-        if recomend_option == 'App':
-            recomend.main()
-        elif recomend_option == 'Gráficos':
-            plots.intro()
-            plots.get_top_25_beer_styles(df_Beer)
-            plots.plot_most_common_beer_bar(df_Beer)
-            plots.plot_most_reviewed_beers(df_Beer)
-            plots.plot_sentiment_distribution(df_Beer)
-            plots.plot_most_common_beer_treemap(df_Beer)
-            plots.plot_sentiment_beer_style_bubble(df_Beer)
-            plots.plot_review_features_correlation(df_Beer)
-            plots.plot_abv_beer_style_box(df_Beer)
-            plots.plot_3d_scatter_aroma_palate_abv(df_Beer)
-            plots.plot_beer_wordcloud(df_Beer)
-    elif page == 'Scraping':
-        scrap.main()
-
-if __name__ == "__main__":
-    main()
+# Ejecutar la función show() cuando se llama al archivo
+if __name__ == '__main__':
+    show()
