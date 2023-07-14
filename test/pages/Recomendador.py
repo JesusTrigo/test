05247553e7a03e6ae428f4d8218a95ba5098689e
JesúsT <83574910+JesusTrigo@users.noreply.github.com>
2023-07-14 16:31:14 +0200

    Update Recomendador.py

--- test/pages/Recomendador.py
+++ test/pages/Recomendador.py
@@ -3,37 +3,6 @@ from pages.pages import plots, recomend
 
 df_Beer = recomend.df_Beer
 
-st.write('<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">',
-
-         # Estilo personalizado para el ícono de flecha
-         '<style>',
-         '.custom-select .fas {',
-         '    font-size: 16px;',
-         '    margin-left: 4px;',
-         '}',
-         '</style>',
-
-         # Contenedor de selección personalizado con ícono de flecha
-         '<div class="custom-select">',
-         '    <select>',
-         '        <option>Opción 1</option>',
-         '        <option>Opción 2</option>',
-         '        <option>Opción 3</option>',
-         '    </select>',
-         '    <i class="fas fa-chevron-down"></i>',  # Icono de flecha hacia abajo de FontAwesome
-         '</div>',
-
-         # Script de JavaScript para inicializar el contenedor de selección personalizado
-         '<script>',
-         '    document.addEventListener("DOMContentLoaded", function() {',
-         '        var select = document.querySelector(".custom-select select");',
-         '        select.addEventListener("change", function() {',
-         '            console.log("Seleccionado:", this.value);',
-         '        });',
-         '    });',
-         '</script>',
-         sep='\n')
-
 def main():
     # Mostrar enlaces para elegir entre plots.py y app.py
     opcion = st.radio("Seleccione una opción:", ["App", "Gráficos"])
